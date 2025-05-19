8620ca47e77d168b4141aec236270482c178caeb
Maxim Kosterov <maxim.kosterov@gmail.com>
2025-05-19 15:15:07 +0300

    adding graph generation

--- clustergenes-cli.py
+++ clustergenes-cli.py
@@ -2,11 +2,32 @@ import clustergenes
 import csv
 import sys
 import numpy as np
+from matplotlib import pyplot as plt
+import os
 
-if len(sys.argv) != 2:
-    print("Usage python3 clustergenes-cli.py [genes csv file]")
+
+def SaveCluster(filename, data, cluster_idx):
+    cluster_data = []
+    for j in cluster_idx:
+        cluster_data.append(data[j])
+    cluster_data = np.array(cluster_data)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    im = ax.imshow(cluster_data.T, aspect="auto", cmap="magma")
+    fig.colorbar(im, ax=ax)
+    fig.savefig(filename)
+
+    plt.close()
+
+if len(sys.argv) != 4:
+    print(
+        "Usage python3 clustergenes-cli.py [genes csv file] [n results] [output folder]"
+    )
+    exit(1)
 
 datafilename = sys.argv[1]
+ntopresults = int(sys.argv[2])
+outfolder = sys.argv[3]
 
 gene_names = []
 patient_data = []
@@ -29,7 +50,7 @@ patient_data = np.array(patient_data)
 
 clustering = clustergenes.GeneCluster(patient_data=patient_data, use_tqdm=True)
 
-for interval in clustering.intervals:
+for i, interval in enumerate(clustering.intervals):
     n = interval["nclusters"]
     p0, p1 = interval["p_interval"]
     plen = interval["p_length"]
@@ -37,10 +58,15 @@ for interval in clustering.intervals:
 
     print("--- Begin ---")
     print(f"n={n:4}, plen={plen:6.2f}, interval=[{p0:6.2f}, {p0:6.2f}]\n")
-    for i, cluster in enumerate(clusters):
-        print(f"Group {i + 1:4}, size={len(cluster):4}")
-        names = [patient_names[i] for i in cluster]
+    for j, cluster in enumerate(clusters):
+        print(f"Group {j + 1:4}, size={len(cluster):4}")
+        names = [patient_names[k] for k in cluster]
         print(" ".join(names))
         print()
     print("End")
     print()
+
+    if i < ntopresults:
+        for j, cluster in enumerate(clusters):
+            filename = os.path.join(outfolder, f"genes-{i + 1}-{j + 1}.eps")
+            SaveCluster(filename, patient_data, cluster)
